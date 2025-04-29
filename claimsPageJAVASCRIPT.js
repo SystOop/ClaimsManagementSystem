fcba3bf84832179c38a96fe2c5e3637d9c76bc95
Domprou AnnaEleni <t8230035@aueb.gr>
2025-04-29 18:11:35 +0300

    Initial layout

---
+++ claimsPageJAVASCRIPT.js
@@ -0,0 +1,7 @@
+function openRequestForm() {
+    document.getElementById('request-popup').style.display = 'block';
+}
+
+function closeRequestForm() {
+    document.getElementById('request-popup').style.display = 'none';
+}
