51bb70a5b3bcac361780e87d7f0c118f7a628435
Domprou AnnaEleni <t8230035@aueb.gr>
2025-04-30 15:18:02 +0300

    Before request

--- claimsPageJAVASCRIPT.js
+++ claimsPageJAVASCRIPT.js
@@ -1,7 +1,18 @@
 function openRequestForm() {
-    document.getElementById('request-popup').style.display = 'block';
+    const select = document.getElementById('team-select');
+    const selectedValue = select.value;
+
+    if (selectedValue === '') {
+        alert("Παρακαλώ επίλεξε μια ομάδα.");
+        return;
+    }
+
+    document.getElementById('popup-title').textContent = `New request to ${selectedValue}`;
+    document.getElementById('popup').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
 }
 
 function closeRequestForm() {
-    document.getElementById('request-popup').style.display = 'none';
+    document.getElementById('popup').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
 }
